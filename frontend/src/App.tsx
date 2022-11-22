c204acc4fe002619ff035a6c3986f7379ac38738
Marcelo Franco <marfns@hotmail.com>
2022-11-22 09:50:48 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./notification/NotificationBotton"
+
 function App() {
   return(
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
